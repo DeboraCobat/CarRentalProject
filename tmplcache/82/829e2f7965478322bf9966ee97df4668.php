53e9e80e2b38c65492fccaafd8b6e127182cd9b4
Debora Bastos <65675553+DeboraCobat@users.noreply.github.com>
2023-04-01 18:06:55 -0400

    footer user interface

--- tmplcache/82/829e2f7965478322bf9966ee97df4668.php
+++ tmplcache/82/829e2f7965478322bf9966ee97df4668.php
@@ -39,16 +39,11 @@ class __TwigTemplate_4b9001700b448bec92476d501525c8be extends Template
     {
         $macros = $this->macros;
         // line 1
-        echo "
-";
-        // line 44
-        echo "
-
-<!DOCTYPE html>
+        echo "<!DOCTYPE html>
 <html>
     <head>
         <title>";
-        // line 49
+        // line 4
         $this->displayBlock('title', $context, $blocks);
         echo "</title>
         <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">
@@ -56,27 +51,27 @@ class __TwigTemplate_4b9001700b448bec92476d501525c8be extends Template
         <link rel=\"stylesheet\" href=\"https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css\">
         <link rel=\"stylesheet\" type=\"text/css\" href=\"/admin.css\">
         ";
-        // line 54
+        // line 9
         $this->displayBlock('addHead', $context, $blocks);
-        // line 55
+        // line 10
         echo "    </head>
      <body>
 
         <div class=\"admin-container\">
             <div class=\"admin-sidebar\">
                 ";
-        // line 60
-        $this->loadTemplate("admin/adminpanel.html.twig", "admin/master.html.twig", 60)->display($context);
-        // line 61
+        // line 15
+        $this->loadTemplate("admin/adminpanel.html.twig", "admin/master.html.twig", 15)->display($context);
+        // line 16
         echo "            </div>
             
             <div class=\"admin-content\">
                  <div class=\"container\">
                     <div class=\"centeredContent\">
                     ";
-        // line 66
+        // line 21
         $this->displayBlock('content', $context, $blocks);
-        // line 67
+        // line 22
         echo "                    </div>
                  </div>
             </div>
@@ -87,49 +82,51 @@ class __TwigTemplate_4b9001700b448bec92476d501525c8be extends Template
         <script src=\"https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js\"></script>
         <script src=\"https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js\"></script>
         ";
-        // line 76
+        // line 31
         $this->displayBlock('javascripts', $context, $blocks);
-        // line 77
+        // line 32
         echo "        
         <div class=\"footer\">
             ";
-        // line 79
+        // line 34
         $this->displayBlock('footer', $context, $blocks);
-        // line 82
+        // line 37
         echo "        </div>
     </body>
-</html>";
+</html>
+
+";
     }
 
-    // line 49
+    // line 4
     public function block_title($context, array $blocks = [])
     {
         $macros = $this->macros;
     }
 
-    // line 54
+    // line 9
     public function block_addHead($context, array $blocks = [])
     {
         $macros = $this->macros;
     }
 
-    // line 66
+    // line 21
     public function block_content($context, array $blocks = [])
     {
         $macros = $this->macros;
     }
 
-    // line 76
+    // line 31
     public function block_javascripts($context, array $blocks = [])
     {
         $macros = $this->macros;
     }
 
-    // line 79
+    // line 34
     public function block_footer($context, array $blocks = [])
     {
         $macros = $this->macros;
-        // line 80
+        // line 35
         echo "                &copy; ";
         echo 2023;
         echo "
@@ -148,12 +145,51 @@ class __TwigTemplate_4b9001700b448bec92476d501525c8be extends Template
 
     public function getDebugInfo()
     {
-        return array (  133 => 80,  129 => 79,  123 => 76,  117 => 66,  111 => 54,  105 => 49,  99 => 82,  97 => 79,  93 => 77,  91 => 76,  80 => 67,  78 => 66,  71 => 61,  69 => 60,  62 => 55,  60 => 54,  52 => 49,  45 => 44,  42 => 1,);
+        return array (  130 => 35,  126 => 34,  120 => 31,  114 => 21,  108 => 9,  102 => 4,  94 => 37,  92 => 34,  88 => 32,  86 => 31,  75 => 22,  73 => 21,  66 => 16,  64 => 15,  57 => 10,  55 => 9,  47 => 4,  42 => 1,);
     }
 
     public function getSourceContext()
     {
-        return new Source("
+        return new Source("<!DOCTYPE html>
+<html>
+    <head>
+        <title>{% block title %}{% endblock %}</title>
+        <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">
+        <!-- Combined CSS -->
+        <link rel=\"stylesheet\" href=\"https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css\">
+        <link rel=\"stylesheet\" type=\"text/css\" href=\"/admin.css\">
+        {% block addHead %}{% endblock %}
+    </head>
+     <body>
+
+        <div class=\"admin-container\">
+            <div class=\"admin-sidebar\">
+                {% include 'admin/adminpanel.html.twig' %}
+            </div>
+            
+            <div class=\"admin-content\">
+                 <div class=\"container\">
+                    <div class=\"centeredContent\">
+                    {% block content %}{% endblock %}
+                    </div>
+                 </div>
+            </div>
+        </div>
+        
+        <!-- CDNs for jQuery, Popper.js and Bootstrap JS -->
+        <script src=\"https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js\"></script>
+        <script src=\"https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js\"></script>
+        <script src=\"https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js\"></script>
+        {% block javascripts %}{% endblock %}
+        
+        <div class=\"footer\">
+            {% block footer %}
+                &copy; {{ 2023 }}
+            {% endblock %}
+        </div>
+    </body>
+</html>
+
 {# <!DOCTYPE html>
 <html>
     <head>
@@ -196,46 +232,6 @@ class __TwigTemplate_4b9001700b448bec92476d501525c8be extends Template
 </body>
 </html>
  #}
-
-
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>{% block title %}{% endblock %}</title>
-        <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">
-        <!-- Combined CSS -->
-        <link rel=\"stylesheet\" href=\"https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css\">
-        <link rel=\"stylesheet\" type=\"text/css\" href=\"/admin.css\">
-        {% block addHead %}{% endblock %}
-    </head>
-     <body>
-
-        <div class=\"admin-container\">
-            <div class=\"admin-sidebar\">
-                {% include 'admin/adminpanel.html.twig' %}
-            </div>
-            
-            <div class=\"admin-content\">
-                 <div class=\"container\">
-                    <div class=\"centeredContent\">
-                    {% block content %}{% endblock %}
-                    </div>
-                 </div>
-            </div>
-        </div>
-        
-        <!-- CDNs for jQuery, Popper.js and Bootstrap JS -->
-        <script src=\"https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js\"></script>
-        <script src=\"https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js\"></script>
-        <script src=\"https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js\"></script>
-        {% block javascripts %}{% endblock %}
-        
-        <div class=\"footer\">
-            {% block footer %}
-                &copy; {{ 2023 }}
-            {% endblock %}
-        </div>
-    </body>
-</html>", "admin/master.html.twig", "/Applications/XAMPP/xamppfiles/htdocs/carrentalproject/templates/admin/master.html.twig");
+", "admin/master.html.twig", "/Applications/XAMPP/xamppfiles/htdocs/carrentalproject/templates/admin/master.html.twig");
     }
 }
