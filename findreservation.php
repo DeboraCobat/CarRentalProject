a0b34cedc52e580940bf03eb01e14ba8e8ced5f8
Debora Bastos <65675553+DeboraCobat@users.noreply.github.com>
2023-04-02 20:47:30 -0400

    scrum

--- findreservation.php
+++ findreservation.php
@@ -2,52 +2,44 @@
 
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
+use DI\Container;
+use Slim\Views\Twig;
+use Slim\Views\TwigMiddleware;
+use Psr\Http\Message\UploadedFileInterface;
 
 
 require_once 'init.php';
 
-// Route for displaying the reservation search form
-$app->get('/findmyreservation', function (Request $request, Response $response) {
+
+//// FINDING RESERVATION
+
+$app->get('/findmyreservation', function (Request $request, Response $response, $args) {
     return $this->get('view')->render($response, 'findmyreservation.html.twig');
 });
 
-// Route for processing the form submission
-$app->post('/findmyreservation', function (Request $request, Response $response) {
-    $data = $request->getParsedBody();
+// $myreservation = DB::queryFirstRow("SELECT * FROM reservations WHERE reservations.customer_id = 9");
 
-    // Retrieve the email and reservation ID from the form data
-    $email = $data['email'];
-    $reservationId = $data['reservation_id'];
+// $myreservation = DB::queryFirstRow("SELECT * FROM reservations WHERE reservations.customer_id = %i", $args['id']);
 
-    // Query the database to find the reservation based on the reservation ID provided
-    $reservation = DB::queryFirstRow("SELECT * FROM reservations WHERE id = %d", $reservationId);
+// $app->post('/findmyreservation', function (Request $request, Response $response) {
+//     $data = $request->getParsedBody();
 
-    // if (!$reservation) {
-    //     // If reservation is not found, return a 404 response
-    //     return $response->withStatus(404)->write('Reservation not found.');
-    // }
+//     // Retrieve the email and reservation ID from the form data
+//     $email = $data['email'];
+//     $reservationId = $data['reservationId'];
 
-    // // Query the database to find the customer based on their email
-    // $customer = DB::queryFirstRow("SELECT * FROM users WHERE email = %s", $email);
+//     // Query the database to find the reservation based on the email and reservation ID provided
+//     $reservation = queryReservation($email, $reservationId);
 
-    // if (!$customer) {
-    //     // If customer is not found, return a 404 response
-    //     return $response->withStatus(404)->write('Customer not found.');
-    // }
+//     // Render the reservation details in a new Twig template
+//     $response->getBody()->write(
+//         $this->get('view')->render(
+//             $response,
+//             'reservation_details.html.twig',
+//             ['reservation' => $reservation]
+//         )
+//     );
 
-    // // Check if the customer ID matches the one in the reservation
-    // if ($customer['id'] !== $reservation['customer_id']) {
-    //     // If customer ID doesn't match, return a 404 response
-    //     return $response->withStatus(404)->write('Reservation not found for this email and reservation ID combination.');
-    // }
-
-    // Retrieve the vehicle details based on the vehicle ID in the reservation
-    $vehicle = DB::queryFirstRow("SELECT * FROM vehicles WHERE id = %d", $reservation['vehicle_id']);
-
-    // Render the reservation details in a new Twig template
-    return $this->get('view')->render($response, 'myreservation.html.twig', [
-        'reservation' => $reservation,
-        'customer' => $customer,
-        'vehicle' => $vehicle
-    ]);
-});
+//     return $response;
+// });
