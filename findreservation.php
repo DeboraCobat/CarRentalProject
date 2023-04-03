2e21fdea47280e97ff9233a06755a1698f8bae54
Debora Bastos <65675553+DeboraCobat@users.noreply.github.com>
2023-04-02 22:08:02 -0400

    findmyreservation

--- findreservation.php
+++ findreservation.php
@@ -14,32 +14,24 @@ require_once 'init.php';
 
 //// FINDING RESERVATION
 
-$app->get('/findmyreservation', function (Request $request, Response $response, $args) {
-    return $this->get('view')->render($response, 'findmyreservation.html.twig');
+$app->post('/findmyreservation', function (Request $request, Response $response) {
+    $data = $request->getParsedBody();
+
+    // Retrieve the email and reservation ID from the form data
+    $email = $data['email'];
+    $reservationId = $data['reservationId'];
+
+    // Query the database to find the reservation based on the email and reservation ID provided
+    $reservation = DB::queryFirstRow("SELECT * FROM reservations WHERE email = %s AND reservation_id = %s", $email, $reservationId);
+
+    // Render the reservation details in a new Twig template
+    $response->getBody()->write(
+        $this->get('view')->render(
+            $response,
+            'myreservation.html.twig',
+            ['reservation' => $reservation]
+        )
+    );
+
+    return $response;
 });
-
-// $myreservation = DB::queryFirstRow("SELECT * FROM reservations WHERE reservations.customer_id = 9");
-
-// $myreservation = DB::queryFirstRow("SELECT * FROM reservations WHERE reservations.customer_id = %i", $args['id']);
-
-// $app->post('/findmyreservation', function (Request $request, Response $response) {
-//     $data = $request->getParsedBody();
-
-//     // Retrieve the email and reservation ID from the form data
-//     $email = $data['email'];
-//     $reservationId = $data['reservationId'];
-
-//     // Query the database to find the reservation based on the email and reservation ID provided
-//     $reservation = queryReservation($email, $reservationId);
-
-//     // Render the reservation details in a new Twig template
-//     $response->getBody()->write(
-//         $this->get('view')->render(
-//             $response,
-//             'reservation_details.html.twig',
-//             ['reservation' => $reservation]
-//         )
-//     );
-
-//     return $response;
-// });
